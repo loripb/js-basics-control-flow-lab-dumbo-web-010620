9e929ab0994b3be61bb9f1dc70379c59635406a3
Lori Boyd <lori.p.boyd@gmail.com>
2020-02-17 15:04:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function scuberGreetingForFeet(rideLength){
 
 function ternaryCheckCity(city){
   // Write your code here!
-  return city === 'NYC' ? 'Ok,sounds good.' : 'No go.';
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip(){
