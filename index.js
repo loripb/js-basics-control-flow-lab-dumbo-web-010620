7068a997ccc9a722ea79cb3384ec89f7a21310a9
Lori Boyd <lori.p.boyd@gmail.com>
2020-02-17 14:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function scuberGreetingForFeet(rideLength){
   // Write your code here!
   if (rideLength <= 400) {
-    console.log('This on is on me!')
+    console.log('This one is on me!')
   }
 }
 
