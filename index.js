67d7b29c244339a59ec5b87dc2193aeba993dcee
Lori Boyd <lori.p.boyd@gmail.com>
2020-02-17 14:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function scuberGreetingForFeet(rideLength){
   // Write your code here!
   if (rideLength <= 400) {
     return 'This one is on me!'
-  } else if (rideLength > 2000) {
+  } else if (rideLength > 2000 && rideLength < 2500) {
     return 'I will gladly take your thirty bucks.'
-  } else if (rideLength > 2500) {
+  } else {
     return 'No can do.'
   }
 }
